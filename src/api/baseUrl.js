bd39141ef933c1fee13c6aa5407818704f4b521f
dxanh <xuananh@MBA-Anh.local>
2020-03-28 18:25:10 +0700

    production deploy

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : ' https://aqueous-sands-71700.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
